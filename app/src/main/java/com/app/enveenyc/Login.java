dbd8177102d5270cf63245b369eae223deb11ae7
Wadgma Masab <wadgmamasab@gmail.com>
2019-10-18 22:12:29 -0400

    Second Commit

--- app/src/main/java/com/app/enveenyc/Login.java
+++ app/src/main/java/com/app/enveenyc/Login.java
@@ -2,22 +2,19 @@ package com.app.enveenyc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import android.view.View;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
     public void goToAnActivity(View view) {
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
